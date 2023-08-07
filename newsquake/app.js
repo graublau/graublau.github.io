b39e2255af169f6628340ce27f67ea3ef252b19a
chr_mandl <callmechristoph@gmail.com>
2023-08-07 20:10:40 +0200

    magic link whitelist

--- newsquake/app.js
+++ newsquake/app.js
@@ -6,22 +6,48 @@ const supabase = createClient(
 // auth
 
  // Handle form submission
- document.getElementById('magicLinkForm').addEventListener('submit', async (event) => {
+//  document.getElementById('magicLinkForm').addEventListener('submit', async (event) => {
+//   event.preventDefault();
+//   const formData = new FormData(event.target);
+//   const email = formData.get('email');
+
+//   try {
+//     const { error } = await supabase.auth.signInWithOtp({ email });
+
+//     if (error) {
+//       throw new Error(error.message);
+//     }
+
+//     alert('A magic link has been sent to your email. Click the link to sign in.');
+//   } catch (error) {
+//     console.error('Magic link login failed:', error.message);
+//     alert('Magic link login failed :-/');
+//   }
+// });
+
+document.getElementById('magicLinkForm').addEventListener('submit', async (event) => {
   event.preventDefault();
   const formData = new FormData(event.target);
   const email = formData.get('email');
 
-  try {
-    const { error } = await supabase.auth.signInWithOtp({ email });
+  // List of whitelisted emails
+  const allowedEmails = ['callmechristoph@gmail.com', 'christoph.mandl@ffg.at', 'mag.christoph.mandl@gmail.com'];
 
-    if (error) {
-      throw new Error(error.message);
+  if (allowedEmails.includes(email)) {
+    try {
+      const { error } = await supabase.auth.signInWithMagicLink({ email });
+
+      if (error) {
+        throw new Error(error.message);
+      }
+
+      alert('A magic link has been sent to your email. Click the link to sign in.');
+    } catch (error) {
+      console.error('Magic link login failed:', error.message);
+      alert('Magic link login failed :-/');
     }
-
-    alert('A magic link has been sent to your email. Click the link to sign in.');
-  } catch (error) {
-    console.error('Magic link login failed:', error.message);
-    alert('Magic link login failed :-/');
+  } else {
+    alert('This email is not whitelisted for magic link login.');
   }
 });
 
